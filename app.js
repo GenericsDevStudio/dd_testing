f6a89e3357e1158bd4e27c5f193e5c630dda948e
yehorbk <e.bublyk1010@gmail.com>
2019-11-07 18:57:10 +0200

    Scripts connected, html updated

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// Environment
 
 const subjectInput = document.getElementById('subject');
 const taskInput = document.getElementById('task');
 const dateInput = document.getElementById('date');
-
-//
-
+const tasksList = document.getElementById('tasks');
+//const 
 
 const homeworkList = [];
 
@@ -13,27 +11,56 @@ function addHomework() {
     const subject = subjectInput.value;
     const task = taskInput.value;
     const date = dateInput.value;
-
     const homework = new Homework(subject, task, date);
+    homeworkList.push(homework);
+    updateList();
+}
 
-    this.homeworkList.push(homework);
+function updateList() {
+    tasksList.innerHTML = '';
+    for (const item of homeworkList) {
+        tasksList.innerHTML += getListItem(item);
+    }
+}
+
+function getListItem(homework) {
+    return `
+        <a href="#" class="list-group-item list-group-item-action" onclick="editHomework()">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">${ homework.subject }</h5>
+                <small>${ homework.date }</small>
+            </div>
+            <p class="mb-1">${ homework.task }</p>
+        </a>
+    `;
 }
 
 function editHomework() {
-
+    console.log(this);
+    const index = this.value;
+    subjectInput.value = homeworkList[index].subject;
+    taskInput.value = homeworkList[index].task;
+    dateInput.value = homeworkList[index].date;
 }
 
 function updateHomework() {
-    
+    const task = taskInput.value;
+    homeworkList[index].task = task;
+    clearFields();
+    updateList();
+}
+
+function clearFields() {
+    subjectInput.value = '';
+    taskInput.value = '';
+    dateInput.value = '';
 }
 
 function deleteHomework() {
     const index = this.value;
     homeworkList.splice(index);
+    updateList();
 }
-
-
-
 
 class Homework {
 
