c714215a48ba0149401c8227355d091f8e59fee8
yehorbk <e.bublyk1010@gmail.com>
2019-11-07 18:13:03 +0200

    Basic fucntions added

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-alert();
+// Environment
+
+const subjectInput = document.getElementById('subject');
+const taskInput = document.getElementById('task');
+const dateInput = document.getElementById('date');
+
+//
+
+
+const homeworkList = [];
+
+function addHomework() {
+    const subject = subjectInput.value;
+    const task = taskInput.value;
+    const date = dateInput.value;
+
+    const homework = new Homework(subject, task, date);
+
+    this.homeworkList.push(homework);
+}
+
+function editHomework() {
+
+}
+
+function updateHomework() {
+    
+}
+
+function deleteHomework() {
+    const index = this.value;
+    homeworkList.splice(index);
+}
+
+
+
+
+class Homework {
+
+    constructor(subject, homework, date) {
+        this.subject = subject;
+        this.homework = homework;
+        this.date = date;
+    }
+
+}
